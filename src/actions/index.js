b9579da326069a254cf8d143ee3c4e3bf6f4557c
Liam Baxter <liam@coffee-design.co.uk>
2019-11-07 15:23:09 +0000

    memoized api calls with lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+// lodash to use memoize
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async (dispatch) => {
@@ -6,3 +8,15 @@ export const fetchPosts = () => async (dispatch) => {
 	// must return plain js objects with type property, thunk allows us to return a function too!
 	dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = (userId) => (dispatch) => {
+	_fetchUser(userId, dispatch);
+};
+
+// must momoize function outside the aciton creator for it to acutally work. _ means its private, dont user anywhere else.
+
+const _fetchUser = _.memoize(async (userId, dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+	dispatch({ type: 'FETCH_USER', payload: response.data });
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import postsReducer from './postsReducer';
+import userReducer from './userReducer';
 
+// combined state of the app
 export default combineReducers({
-	posts: postsReducer
+	posts: postsReducer,
+	users: userReducer
 });
